eb53b9bf9f8a4974f4350d14a5915149dec542e2
alvarotorrezc <alvarotorrezchambi@gmail.com>
2025-10-12 22:55:30 -0400

    commit: subir archivo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./home/home').then(m => m.Home)
+      }
+    ]
+  }
+];
